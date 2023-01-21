d98c7c9597c3ed93fde6c50445c05190b5cca406
Brian Buddhadasa <brianbuddhadasa@gmail.com>
2023-01-21 18:35:34 -0500

    loop and append acct titles

--- index.js
+++ index.js
@@ -1,54 +1,68 @@
 const accounts = [
-    {
-        id: 1,
-        title: "Main Account",
-        balance: "6700.56",
-        spendings: [
-            {
-                category: "Rent",
-                spent: "1450"
-            },
-            {
-                category: "Groceries",
-                spent: "564"
-            },
-            {
-                category: "Restaurants",
-                spent: "123"
-            },
-            {
-                category: "Transport",
-                spent: "81"
-            },
-            {
-                category: "Internet",
-                spent: "50"
-            }
-        ]
-    },
-    {
-        id: 2,
-        title: "Expenses",
-        balance: "5134.63",
-        spendings: [
-            {
-                category: "Netflix",
-                spent: "19.99"
-            },
-            {
-                category: "HBO Max",
-                spent: "14.99"
-            },
-            {
-                category: "Setapp",
-                spent: "9.99"
-            }
-        ]
-    },
-    {
-        id: 3,
-        title: "Savings",
-        balance: "36500.12",
-        spendings: []
-    }
-]
+  {
+    id: 1,
+    title: "Main Account",
+    balance: "6700.56",
+    spendings: [
+      {
+        category: "Rent",
+        spent: "1450",
+      },
+      {
+        category: "Groceries",
+        spent: "564",
+      },
+      {
+        category: "Restaurants",
+        spent: "123",
+      },
+      {
+        category: "Transport",
+        spent: "81",
+      },
+      {
+        category: "Internet",
+        spent: "50",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Expenses",
+    balance: "5134.63",
+    spendings: [
+      {
+        category: "Netflix",
+        spent: "19.99",
+      },
+      {
+        category: "HBO Max",
+        spent: "14.99",
+      },
+      {
+        category: "Setapp",
+        spent: "9.99",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Savings",
+    balance: "36500.12",
+    spendings: [],
+  },
+];
+
+const accountEL = document.querySelectorAll(".account");
+const accountContainerEl = document.querySelectorAll(".accounts-container");
+const accountNameEl = document.querySelectorAll(".account-name");
+
+for (let [index, item] of accounts.entries()) {
+  accountNameEl[index].innerHTML = `${item.title}`;
+}
+
+for (let i = 0; i < accountEL.length; i++) {
+  accountEL[i].addEventListener("click", function () {
+    console.log("click");
+  });
+}
